5a8ad1664ef8d0c909a81d272eb8d35c96d8b8ed
Anup Gupta <84306754+anuup22@users.noreply.github.com>
2023-09-20 18:40:11 +0000

    lecture 2  completed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JavaScript code as newer version
+
+// alert("Hello")       //we are using nodejs, not browser
+
+let name = "Anup"
+let age = 20 
+let isLoggedIN = true
+
+let state;          //undefined 
+let temp = null     //standalone empty value
+//symbol = unique value
+
+console.log(typeof age);
+console.log(typeof null); //null is an object
+console.log(typeof undefined); //undefined
