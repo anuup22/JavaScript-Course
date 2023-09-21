987bcc476a8513e043f4e79e5931a7efb0ae86bf
Anup Gupta <84306754+anuup22@users.noreply.github.com>
2023-09-21 17:03:22 +0000

    lecture 4 is done

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,10 @@
+/*
+console.log(null > 0); //for comparison <,>,>== and <== null becomes 0
+console.log(null == 0); //equlity checks work differently
+console.log(null >= 0);
+*/
+
+// ===
+
+console.log("2" == 2);  //datatype is being converted to int
+console.log("2" === 2); //datatype is also checked strictly
