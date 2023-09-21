6c7aa1097dc5a07aed6de4b27a697863b341f6f1
Anup Gupta <84306754+anuup22@users.noreply.github.com>
2023-09-21 17:56:05 +0000

    stack and heap done

---
+++ 01_basics/04_memory.js
@@ -0,0 +1,10 @@
+//Stack (Primitive)
+//Heap (Non-premitive)
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@paytm"
+}
+
+let userTwo = userOne 
+//if we changed anything in userTwo, same will be changed in userOne
