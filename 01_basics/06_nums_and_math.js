83afcfdb2334da66616821800052426b9bca7abd
Anup Gupta <84306754+anuup22@users.noreply.github.com>
2023-09-22 17:48:56 +0000

    lecture 6 started

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,11 @@
+const score = 400
+const balance = new Number(100)
+
+console.log(score);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2);
+
+const anotherNumber = 23.57971
+console.log(anotherNumber.toPrecision(4));
